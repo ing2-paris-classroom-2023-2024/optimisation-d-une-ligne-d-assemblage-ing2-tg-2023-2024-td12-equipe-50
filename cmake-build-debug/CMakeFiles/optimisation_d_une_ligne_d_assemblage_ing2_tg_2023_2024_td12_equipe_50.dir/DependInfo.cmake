67ae95e838620a44d4f233b5ce5b8ad6123c154a
Cheikh <bambafall@MacBook-Air-de-Cheikh.local>
2023-12-08 12:20:59 +0100

    ajout des fichiers utiles

---
+++ cmake-build-debug/CMakeFiles/optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-td12-equipe-50/main.c" "/tmp/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-td12-equipe-50/cmake-build-debug/CMakeFiles/optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.dir/main.c.o"
+  "optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.pdb"
+  "optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/optimisation_d_une_ligne_d_assemblage_ing2_tg_2023_2024_td12_equipe_50.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
